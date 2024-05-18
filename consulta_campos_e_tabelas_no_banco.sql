b2e1d8dbe2addc91cda16a5f27fd8c94646641bf
JOSÉ ALVES FERREIRA NETO <128003646+alvesfneto@users.noreply.github.com>
2024-05-18 07:22:20 -0300

    Atualizações

--- consulta_campos_e_tabelas_no_banco.sql
+++ consulta_campos_e_tabelas_no_banco.sql
@@ -3,11 +3,13 @@ a.name as Tabela,
 a.object_id, 
 b.name as Coluna,
 column_id
-from sys.tables a 
-inner join sys.columns b on a.object_id=b.object_id
+from DB_EDUCACAO_1_PRODAM.SE1426_SS.sys.tables a 
+inner join DB_EDUCACAO_1_PRODAM.SE1426_SS.sys.columns b on a.object_id=b.object_id
 --where a.name like '%CEU%'
 --ou 
-where b.name like '%distr%'
+where b.name like '%SG_DISTRITO_MEC%'
 
 
-select * from SME_ESCOLA where ENDERECO like '%luis go%'
+select * from SME_ESCOLA where ENDERECO like '%INHAIBA%'
+
+select * from SME_ESCOLA where COD_ESC ='007816'
